facd295a651bf279824de9999059198837769fd0
Nick Hand <nicholas.adam.hand@gmail.com>
2019-04-19 13:49:07 -0400

    add limit keyword

--- esri2sf/esri2sf.py
+++ esri2sf/esri2sf.py
@@ -9,15 +9,14 @@ def _get_json_safely(response):
     Check for JSON response errors, and if all clear, 
     return the JSON data
     """
-    response.raise_for_status()  # raise exceptions
     json = response.json()  # get the JSON
-    if 'error' in json:
-        raise ValueError("Error: %s" % json['error']['details'][0])
+    if "error" in json:
+        raise ValueError("Error: %s" % json["error"])
 
     return json
 
 
-def get(url, fields=None, where=None):
+def get(url, fields=None, where=None, limit=None):
     """
     Scrape features from a ArcGIS Server REST API and return a
     geopandas GeoDataFrame.
@@ -26,12 +25,14 @@ def get(url, fields=None, where=None):
     ----------
     url : str
         the REST API url for the Feature Service
-    fields : list of str
+    fields : list of str, optional
         the list of fields to include; the default behavior ('None') 
         returns all fields
-    where : str
+    where : str, optional
         a string specifying the selection clause to select a subset of 
         data; the default behavior ('None') selects all data
+    limit : int, optional
+        limit the returned data to this many features
 
     Example
     -------
@@ -39,7 +40,6 @@ def get(url, fields=None, where=None):
     >>> gdf = esri2sf.get(url, fields=['MAPNAME'], where="MAPNAME='Chestnut Hill'")
     >>> gdf
     """
-
     # default behavior matches all features
     if where is None:
         where = "1=1"
@@ -55,16 +55,19 @@ def get(url, fields=None, where=None):
 
     # get the object IDs safely
     json = _get_json_safely(response)
-    ids = json['objectIds']
+    ids = json["objectIds"]
+
+    # impose the limit
+    if limit is not None:
+        ids = ids[:limit]
 
     # get raw features
-    params = dict(objectIds=", ".join(map(str, ids)),
-                  f="json",
-                  outSR="4326",
-                  outFields=fields)
+    params = dict(
+        objectIds=", ".join(map(str, ids)), f="json", outSR="4326", outFields=fields
+    )
     response = requests.get(queryURL, params=params)
     json = _get_json_safely(response)
 
     # convert to GeoJSON and return
-    geojson = [arcgis2geojson(f) for f in json['features']]
+    geojson = [arcgis2geojson(f) for f in json["features"]]
     return gpd.GeoDataFrame.from_features(geojson, crs={"init": "epsg:4326"})
