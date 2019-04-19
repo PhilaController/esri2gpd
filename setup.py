abf1b50023c5dbc6bac69ce7b447268ad1ad6257
Nick Hand <nicholas.adam.hand@gmail.com>
2019-04-19 13:48:54 -0400

    formatting with black

--- setup.py
+++ setup.py
@@ -3,20 +3,21 @@ from setuptools import setup
 
 def find_version(path):
     import re
+
     # path shall be a plain ascii text file.
-    s = open(path, 'rt').read()
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              s, re.M)
+    s = open(path, "rt").read()
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", s, re.M)
     if version_match:
         return version_match.group(1)
     raise RuntimeError("Version not found")
 
 
-setup(name="esri2sf",
-      version=find_version('esri2sf/__init__.py'),
-      author="Nick Hand",
-      maintainer="Nick Hand",
-      maintainer_email="nick.hand@phila.gov",
-      description="A Python utility to scrape features from the ArcGIS Server REST API and return a geopandas GeoDataFrame",
-      license='MIT',
-      )
+setup(
+    name="esri2sf",
+    version=find_version("esri2sf/__init__.py"),
+    author="Nick Hand",
+    maintainer="Nick Hand",
+    maintainer_email="nick.hand@phila.gov",
+    description="A Python utility to scrape features from the ArcGIS Server REST API and return a geopandas GeoDataFrame",
+    license="MIT",
+)
