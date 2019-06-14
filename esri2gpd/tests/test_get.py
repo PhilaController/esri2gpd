eabe8ba680c17f1d4e30ee9e468675982857b5f7
Nick Hand <nicholas.adam.hand@gmail.com>
2019-06-14 12:27:45 -0400

    update tests

--- esri2gpd/tests/test_get.py
+++ esri2gpd/tests/test_get.py
@@ -13,3 +13,9 @@ def test_fields():
     gdf = esri2gpd.get(url, fields=["MAPNAME"])
     assert all(col in gdf.columns for col in ["MAPNAME", "geometry"])
 
+
+def test_where():
+    url = "https://services.arcgis.com/fLeGjb7u4uXqeF9q/ArcGIS/rest/services/Philly_Neighborhoods/FeatureServer/0"
+    gdf = esri2gpd.get(url, fields=["MAPNAME"], where="MAPNAME='Chestnut Hill'")
+    assert (gdf["MAPNAME"] == "Chestnut Hill").all()
+
