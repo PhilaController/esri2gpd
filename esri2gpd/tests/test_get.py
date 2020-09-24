cba9c76081b81e6d1369cd023ba57349b6f938c3
Nick Hand <nicholas.adam.hand@gmail.com>
2020-09-24 11:00:39 -0400

    fix tests

--- esri2gpd/tests/test_get.py
+++ esri2gpd/tests/test_get.py
@@ -3,19 +3,19 @@ import pytest
 
 
 def test_limit():
-    url = "https://services.arcgis.com/fLeGjb7u4uXqeF9q/ArcGIS/rest/services/Philly_Neighborhoods/FeatureServer/0"
-    gdf = esri2gpd.get(url, fields=["MAPNAME"], limit=1)
+    url = "https://services.arcgis.com/fLeGjb7u4uXqeF9q/arcgis/rest/services/Philadelphia_ZCTA_2018/FeatureServer/0"
+    gdf = esri2gpd.get(url, fields=["zip_code"], limit=1)
     assert len(gdf) == 1
 
 
 def test_fields():
-    url = "https://services.arcgis.com/fLeGjb7u4uXqeF9q/ArcGIS/rest/services/Philly_Neighborhoods/FeatureServer/0"
-    gdf = esri2gpd.get(url, fields=["MAPNAME"])
-    assert all(col in gdf.columns for col in ["MAPNAME", "geometry"])
+    url = "https://services.arcgis.com/fLeGjb7u4uXqeF9q/arcgis/rest/services/Philadelphia_ZCTA_2018/FeatureServer/0"
+    gdf = esri2gpd.get(url, fields=["zip_code"])
+    assert all(col in gdf.columns for col in ["zip_code", "geometry"])
 
 
 def test_where():
-    url = "https://services.arcgis.com/fLeGjb7u4uXqeF9q/ArcGIS/rest/services/Philly_Neighborhoods/FeatureServer/0"
-    gdf = esri2gpd.get(url, fields=["MAPNAME"], where="MAPNAME='Chestnut Hill'")
-    assert (gdf["MAPNAME"] == "Chestnut Hill").all()
+    url = "https://services.arcgis.com/fLeGjb7u4uXqeF9q/arcgis/rest/services/Philadelphia_ZCTA_2018/FeatureServer/0"
+    gdf = esri2gpd.get(url, fields=["zip_code"], where="zip_code=19123")
+    assert (gdf["zip_code"] == 19123).all()
 
