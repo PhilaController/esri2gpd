ce2f6295a5912593366d500a9858990b52b1eb25
Nick Hand <nicholas.adam.hand@gmail.com>
2020-10-01 16:00:48 -0400

    add py3.8

--- deploy_anaconda.sh
+++ deploy_anaconda.sh
@@ -5,7 +5,7 @@ set -e
 for packageName in "arcgis2geojson" "esri2gpd";
 do
     echo "Building conda recipe..."
-    conda build --variants "{python: [3.6, 3.7]}" conda-recipe/$packageName
+    conda build --variants "{python: [3.6, 3.7, 3.8]}" conda-recipe/$packageName
 
     echo "Converting conda package..."
     conda convert --platform all $HOME/miniconda/conda-bld/linux-64/$packageName-*.tar.bz2 --output-dir conda-bld/
