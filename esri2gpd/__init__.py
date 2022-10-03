09092bc8708b4b624cbcf88263d2d45f44eea4d8
Nick Hand <nicholas.adam.hand@gmail.com>
2022-10-03 13:50:06 -0400

    update dependencies

--- esri2gpd/__init__.py
+++ esri2gpd/__init__.py
@@ -1,9 +1,4 @@
-try:
-    from importlib.metadata import version
-except ImportError:
-    # Running on pre-3.8 Python; use importlib-metadata package
-    from importlib_metadata import version
-
+from importlib.metadata import version
 from .core import get
 
 __version__ = version(__package__)
